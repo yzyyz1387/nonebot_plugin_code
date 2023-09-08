f071976be87fc65a7f7e707df71970f0e173337b
yzyyz1387 <1796031384@qq.com>
2023-09-08 18:46:58 +0800

    :package: add Metadata

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="nonebot_plugin_code",
-    version="0.0.6",
+    version="0.0.7",
     author="yzyyz1387",
     author_email="youzyyz1384@qq.com",
     keywords=("pip", "nonebot2", "nonebot", "nonebot_plugin"),
